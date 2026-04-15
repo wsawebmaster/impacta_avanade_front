be21e33eb121349606eda605e3e63e0cea65d7a6
Wagner Andrade <wsawebmaster@yahoo.com.br>
2026-04-15 20:36:53 -0300

    feat: Classes: Construtor e Métodos

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 class BankAccount {
+    constructor(accNumber, initialFunds) {
+        this.accNumber = accNumber;
+        this.funds = initialFunds !== null && initialFunds !== void 0 ? initialFunds : 0;
+    }
 }
 function main() {
-    const a1 = new BankAccount();
-    const a2 = new BankAccount();
-    a1.accNumber = 123;
-    a2.accNumber = 567;
+    const a1 = new BankAccount(1, 10);
+    const a2 = new BankAccount(2, 0);
+    const a3 = new BankAccount(3, 20);
     console.log(a1);
     console.log(a2);
+    console.log(a3);
 }
 main();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 class BankAccount {
-    accNumber: number | undefined;
+    accNumber: number;
+    funds: number;
+
+    constructor(accNumber: number, initialFunds?: number) {
+        this.accNumber = accNumber;
+        this.funds = initialFunds ?? 0;
+    }
 }
 
 function main() {
-    const a1 = new BankAccount();
-    const a2 = new BankAccount();
+    const a1 = new BankAccount(1, 10);
+    const a2 = new BankAccount(2, 0);
+    const a3 = new BankAccount(3, 20);
 
-    a1.accNumber = 123;
-    a2.accNumber = 567;
     console.log(a1);
     console.log(a2);
+    console.log(a3);
 }
 
 main();

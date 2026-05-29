2a37917b581b7ef1daffd5788068358da0ab9728
Wagner Andrade <wsawebmaster@yahoo.com.br>
2026-05-28 22:13:25 -0300

    feat: Polimorfismo Promises async e await

--- src/asyncProgram.ts
+++ src/asyncProgram.ts
@@ -1,10 +1,10 @@
-import { createReadStream } from "fs";
+import { readFile } from "fs";
 
 function log(msg: string, buffer: Array<string> | null = null) {
     const _msg = `${msg} - ${new Date(Date.now())}`;
     if (buffer) {
         buffer.push(_msg);
-        if(buffer.length > 1000) {
+        if (buffer.length > 1000) {
             log('Buffer atingiu o limite ...');
             log('_msg');
             buffer.length = 0;
@@ -27,18 +27,20 @@ log('d');
 //setInterval(() => log('repeating'), 1000)
 
 log('Lendo arquivo grande...');
-// readFile('src/asyncProgram.ts', 'utf-8', (err, data) => {
-    //         console.error('Erro ao ler arquivo', err);
-    //         console.log('Conteúdo do arquivo', data);
-    //         log(`data.length: ${data.length}`);
-    //     });
-    
-    const bufferStr: Array<string> = [];
-    const readStream = createReadStream('./theFile.txt', 'utf-8');
-    readStream.on('data', st => log(`data.length: ${st.length}, bufferStr`));
-    readStream.on('end', () => {
-        log('Fechando stream...');
-        readStream.close();
-    });
 
-    setTimeout(() => log('Leitura de arquivo grande finalizada'), 7000)
+const p = new Promise<string>((resolve, reject) => {
+    readFile('./theFile.txt', { encoding: 'utf-8' }, (err, data) => {
+        if (err) {
+            reject(err);
+        } else {
+            resolve(`${data.length}`);
+        }
+    })
+});
+
+p
+    .then((x) => log(x))
+    .catch((e) => console.error(e))
+    .finally(() => log('leitura finalizada...'));
+
+setTimeout(() => log('Leitura de arquivo grande finalizada'), 7000)
